f7683d0bbcd00bc2d8cd6cb713965859f8a2a255
Anton <scoutrul@mail.ru>
2018-03-28 00:42:47 +0300

    production v1

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ var paths = {
 	sass: 'src/**/*.scss',
 	js: 'src/js/script.js',
 	data: 'data.json',
-	jade: ['src/jade/index.jade','src/jade/privacypolicy.jade']
+	jade: ['src/jade/index.jade','src/jade/privacypolicy.jade','src/jade/termsofservice.jade']
 };
 
 var processors = [require('postcss-focus'), cssnano ];
